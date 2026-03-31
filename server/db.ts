140d29f81adab52683988227464d0f2d6c0866ec
bmasmhj <bmasmhj@gmail.com>
2026-03-31 07:32:55 +0545

    Allow all origin for testing

--- server/db.ts
+++ server/db.ts
@@ -2,9 +2,10 @@ import pg from 'pg';
 
 const pool = new pg.Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: process.env.DATABASE_URL?.includes('sslmode=require') ? { rejectUnauthorized: false } : false
 });
 
-pool.on('error', (err) => {
+pool.on('error', (err: Error) => {
   console.error('Unexpected database error:', err);
 });
 
